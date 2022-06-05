8d1fdda75235d4a98c010168064e713c708412f3
David Fuenmayor <davfuenmayor@gmail.com>
2022-06-05 22:28:41 +0200

    add some proofs

--- Topology/compactness.thy
+++ Topology/compactness.thy
@@ -18,13 +18,13 @@ lemma FBUP_equiv: "join_closed S \<Longrightarrow> BUP S = FUP S" oops (*Exercis
 
 (**BIP-BUP and FIP-FUP are dual in a sense*)
 lemma BIUP_dual1: "(\<not>BIP S) = BUP S\<^sup>-" using BA_deMorgan1 BA_deMorgan2 unfolding BIP_def BUP_def by (smt (z3) BA_dn L13 L14 L2 L4 L5 L9 compl_def dom_compl_def setequ_char setequ_def setequ_equ subset_def)
-lemma BIUP_dual2: "(\<not>BUP S) = BIP S\<^sup>-" using BA_deMorgan1 BA_deMorgan2 unfolding BIP_def BUP_def by (smt (z3) BA_dn L13 L14 L2 L4 L5 L9 compl_def dom_compl_def setequ_char setequ_def setequ_equ subset_def)
+lemma BIUP_dual2: "(\<not>BUP S) = BIP S\<^sup>-" by (metis BIUP_dual1 dom_compl_invol)
 
-lemma FIUP_dual1: "(\<not>FIP S) = FUP S\<^sup>-" oops (*Exercise: prove this (under minimal conditions)*)
-lemma FIUP_dual2: "(\<not>FUP S) = FIP S\<^sup>-" oops (*Exercise: prove this (under minimal conditions)*)
+lemma FIUP_dual1: "(\<not>FIP S) = FUP S\<^sup>-" using dom_compl_1to1 finite1to1 by (smt (z3) FIP_def FUP_def bottom_def compl_def dom_compl_def dom_compl_invol iDM_a iDM_b setequ_char top_def)
+lemma FIUP_dual2: "(\<not>FUP S) = FIP S\<^sup>-" by (metis FIUP_dual1 dom_compl_invol)
 
 
-(**Below we can state several definitions of compactness and show them equivalent 
+(**Below we can state several definitions of compactness and show them equivalent
  (eventually modulo certain conditions on \<C>). We can also employ BIP/BUP for FIP/FUP.*)
 
 (**The definition of compactness using closed sets*)
@@ -36,10 +36,9 @@ definition compact_op::"('a \<sigma>,'a \<sigma>)\<phi> \<Rightarrow> bool" ("co
   where "compact\<^sup>o\<^sup>p \<C> \<equiv> \<forall>S. S \<sqsubseteq> Op[\<C>] \<and> \<^bold>\<Or>S \<^bold>\<approx> \<^bold>\<top> \<longrightarrow> FUP S"
 
 (**Both definitions above are equivalent (without assuming any condition on \<C>)*)
-lemma "compact\<^sup>c\<^sup>l \<C> = compact\<^sup>o\<^sup>p \<C>" oops (*Exercise: prove this (under minimal conditions)*)
+lemma "compact\<^sup>c\<^sup>l \<C> = compact\<^sup>o\<^sup>p \<C>" unfolding compact_cl_def compact_op_def by (smt (verit) ClOpdual FIUP_dual1 FIUP_dual2 OpCldual bottom_def compl_def dom_compl_def iDM_a iDM_b setequ_char top_def) 
 
 (**Exercise: define and interrelate other definitions of compactness*)
-
 (**Exercise: use compactness to further relate different separation axioms*)
 
 end
